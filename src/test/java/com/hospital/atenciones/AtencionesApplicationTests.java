9d4da2802c5871e01bbfd2d6b3ade5c2346bdd6b
DeuCabrex <be.cabrerad@duocuc.cl>
2026-04-09 10:28:12 -0400

    Agregar la database a un archivo

---
+++ src/test/java/com/hospital/atenciones/AtencionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.atenciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtencionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
