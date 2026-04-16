f92132fe9f1dc56508477575d039f4dc6d98eeeb
DeuCabrex <be.cabrerad@duocuc.cl>
2026-04-16 10:42:14 -0400

    Se crea Paciente con su Repositorio y se hace la api Exception

---
+++ src/main/java/com/hospital/atenciones/models/Paciente.java
@@ -0,0 +1,58 @@
+package com.hospital.atenciones.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "pacientes")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_paciente")
+    private Long id_paciente;
+
+
+    @NotBlank(message = "El campo rut no puede ser vacio")
+    @Column(unique = true, nullable = false)
+    @Pattern(regexp = "\\d{1,8}-[\\dKk]", message = "El formato del run tiene que ser XXXXXXXX-X")
+
+    private String rut;
+
+    @NotBlank(message = "El campo nombres no puede ser vacio")
+    @Column(nullable = false)
+    private String nombres;
+
+
+    @NotBlank(message = "El campo apellidos no puede ser vacio")
+    @Column(nullable = false)
+    private String apellidos;
+
+
+    @NotNull(message = "El campo de fecha nacimiento no puede ser vacio")
+    @Column(nullable = false)
+    private LocalDate fechaNacimiento;
+
+    @Email(message = "El correo tiene que tener formato de correo")
+    @NotBlank(message = "El correo no puede ser vacio")
+    @Column(nullable = false, unique = true)
+    private LocalDate correoElectronico;
+
+    @Embedded
+    private Audit audit = new Audit();
+
+}
---
+++ src/main/java/com/hospital/atenciones/repositories/PacienteRepository.java
@@ -0,0 +1,17 @@
+package com.hospital.atenciones.repositories;
+
+import com.hospital.atenciones.models.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+
+    Optional<Paciente> findByRut(String rut);
+
+    Optional<Paciente> findByCorreo(String coreo);
+
+
+}
